e35a77a85abfa451cc3351d8c3a9805667056943
nikita.makarov <nikita.makarov@odnoklassniki.ru>
2014-09-18 10:32:58 +0400

    yet another fix

--- src/main/java/Crasher.java
+++ src/main/java/Crasher.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class Crasher extends HttpServlet {
 
@@ -12,7 +13,7 @@ public class Crasher extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         long time = Long.parseLong(Optional.fromNullable(req.getParameter("time")).or("0"));
         try {
-            Thread.sleep(time);
+            Thread.sleep(TimeUnit.SECONDS.toMillis(time));
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
